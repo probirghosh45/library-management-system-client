f9da743d2d691a1847059a6ff7d2f7aac0d52ff1
Probir Ghosh <probirghosh@outlook.com>
2025-08-13 03:36:35 +0600

    :hammer: API URL added

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,3 @@
-import { Link } from "react-router-dom";
-
 export default function HomePage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-[80vh] text-center px-4">
